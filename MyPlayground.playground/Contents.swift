16623f3f4f886ca814e9cb63a4fe13104dfde7d4
AntonGuzenko <=>
2022-01-31 13:12:59 +0200

    creating class

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,2 +1,35 @@
 import UIKit
 
+/*let student1 = (name: "Alex", age: 29)
+let student2 = (name: "Bob", age: 22)
+ 
+ //creating students via tuples
+
+student1.name
+student2.name
+
+let student3 = (nam: "Sam", ag: 23)
+student3.nam
+ */
+
+class Student {
+    var name : String
+    var age : Int
+    
+    init() {
+        name = "No name"
+        age = 20
+    }
+    
+    init(name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }
+}
+ 
+let student1 = Student()
+student1.age = 33
+student1.name = "Anton"
+
+let student2 = Student(name: "Bob", age: 18)
+
